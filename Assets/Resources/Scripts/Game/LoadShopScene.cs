03422ff57f27d269b30fa829e47c9c52be2e0666
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2023-08-17 15:22:35 +0200

    Added exit button Fixed tagging issue

--- Assets/Resources/Scripts/Game/LoadShopScene.cs
+++ Assets/Resources/Scripts/Game/LoadShopScene.cs
@@ -29,31 +29,6 @@ public class LoadShopScene : MonoBehaviour
         get => levelDoneFlag;
         set => levelDoneFlag = value;
     }
-
-    public void ViewTutorialScene()
-    {
-        SceneManager.LoadScene("Scenes/0 - Tutorial", LoadSceneMode.Single);
-    }
-    public void ViewLevelOneScene()
-    {
-        SceneManager.LoadScene("Scenes/1 - Very Easy", LoadSceneMode.Single);
-    }
-    public void ViewLevelTwoScene()
-    {
-        SceneManager.LoadScene("Scenes/2 - Easy", LoadSceneMode.Single);
-    }
-    public void ViewLevelThreeScene()
-    {
-        SceneManager.LoadScene("Scenes/3 - Medium", LoadSceneMode.Single);
-    }
-    public void ViewLevelFourScene()
-    {
-        SceneManager.LoadScene("Scenes/4 - Hard", LoadSceneMode.Single);
-    }
-    public void ViewLevelFiveScene()
-    {
-        SceneManager.LoadScene("Scenes/5 - Expert", LoadSceneMode.Single);
-    }
     private void Awake()
     {
         if (!f)
@@ -65,44 +40,19 @@ public class LoadShopScene : MonoBehaviour
 
     private void Start()
     {
-        if (this.gameObject.CompareTag("EditorOnly"))
+        if (this.gameObject.CompareTag("GameController"))
         {
             done0 = f.clearFlags[0];
             done01 = f.clearFlags[1];
             done012 = f.clearFlags[2];
             done0123 = f.clearFlags[3];
             done01234 = f.clearFlags[4];
-        
-            Level1Button.gameObject.SetActive(false);
-            Level2Button.gameObject.SetActive(false);
-            Level4Button.gameObject.SetActive(false);
-            Level3Button.gameObject.SetActive(false);
-            Level5Button.gameObject.SetActive(false);
-        
-            if (done0)
-            {
-                Level1Button.gameObject.SetActive(true);
-
-                if (done01)
-                {
-                    Level2Button.gameObject.SetActive(true);
-
-                    if (done012)
-                    {
-                        Level3Button.gameObject.SetActive(true);
-
-                        if (done0123)
-                        {
-                            Level4Button.gameObject.SetActive(true);
-
-                            if (done01234)
-                            {
-                                Level5Button.gameObject.SetActive(true);
-                            }
-                        }
-                    }
-                }
-            }
+            
+            Level1Button.gameObject.SetActive(done0);
+            Level2Button.gameObject.SetActive(done01);
+            Level3Button.gameObject.SetActive(done012);
+            Level4Button.gameObject.SetActive(done0123);
+            Level5Button.gameObject.SetActive(done01234);
         }
     }
 
@@ -114,12 +64,6 @@ public class LoadShopScene : MonoBehaviour
             f.clearFlags[levelDoneFlag] = true;
         }
         SceneManager.LoadScene("Main Screen");
-    }
-
-    private bool IntToBool(int i)
-    {
-        //returns false if i=0, true otherwise
-        return i != 0;
     }
 }
 
